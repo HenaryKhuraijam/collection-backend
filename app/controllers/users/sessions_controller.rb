25a5caf30a98dd074ff3326b919d7da1672c2608
AngomRanjan <angomranjan@gmail.com>
2024-02-13 02:04:08 +0530

    Add session destroy method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,8 +8,22 @@ class Users::SessionsController < Devise::SessionsController
   def respond_with(resource, options={})
   render json: {
       status:200,
-      message: 'Signed in successfully',
+      message: 'Login in successfull',
       user: resource
     }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+    Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: { status: 200, message: 'Signed out successfully' }, status: :ok
+    else
+      render json: {
+        status:401,
+        message: 'User not active'
+      }, status: :unauthorized
+    end
   end
 end
