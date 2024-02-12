4f106bed53b7e2a9112bf2fb05c48097018a9d3a
AngomRanjan <angomranjan@gmail.com>
2024-02-13 00:04:02 +0530

    Fix  Lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   def as_json(options = {})
     super(options.merge(methods: [:avatar]))
   end
-     
+
   def avatar
     avatar_img.attached? ? Rails.application.routes.url_helpers.rails_blob_url(avatar_img) : nil
   end
