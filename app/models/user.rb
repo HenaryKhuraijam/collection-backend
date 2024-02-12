9841d3196945f7ea7ec594c9fc7730af6c0da9d6
AngomRanjan <angomranjan@gmail.com>
2024-02-13 01:35:54 +0530

    Install Devise Jwt, Include JTIMatcher in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   has_one_attached :avatar_img
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def jwt_payload
+    super
+  end
 
   def as_json(options = {})
     super(options.merge(methods: [:avatar]))
